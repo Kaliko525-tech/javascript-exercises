53ff51fc520c5fd06f99bf29fef5594745a7e62e
Kalok525 <kalvinhamilton16@gmail.com>
2023-02-22 20:05:07 +0200

    Passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(String) {
+    let splitString = String.split("");
+    let revString = splitString.reverse();
+    let newString = revString.join('')
+    return newString
+    
 };
+
+reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
